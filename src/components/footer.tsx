3587d95bef749d8a45ef6182c1883ea520ba4ffa
EndoTaishi <taimei0405@gmail.com>
2023-06-26 13:32:43 +0900

    footerを修正

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,8 @@
 import Container from './container'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import {faTwitter, faGithub,} from '@fortawesome/free-brands-svg-icons'
-library.add(faTwitter, faGithub)
+import {faTwitter, faGithub, faDribbble,} from '@fortawesome/free-brands-svg-icons'
+library.add(faTwitter, faGithub, faDribbble, )
 
 
 const Footer = () => {
@@ -12,6 +12,14 @@ const Footer = () => {
         <div className="flex justify-between items-center mt-20 py-20 border-t border-paleyYellow-dark">
           <p>©2023 Taishi Endo</p>
           <div className="flex">
+            <a 
+                href="https://note.com/en_tai" 
+                className='p-2 mr-2' 
+                target='_blank' 
+                rel='noopener noreferrer'
+            >
+                <FontAwesomeIcon icon={faDribbble} width="24" />
+            </a>
             <a
               href="https://twitter.com/endotaishi_"
               className="p-2 mr-2"
