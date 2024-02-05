0f79d294b8216438fa5c5cf34755db840ad3fb82
Taishi Endo <82435332+EndoTaishi@users.noreply.github.com>
2024-02-06 02:58:53 +0900

    Refactor profile component layout

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -3,14 +3,14 @@ import { Skeleton, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
 
 export function Profile() {
   return (
-    <div className='flex justify-evenly'>
-        <div className='flex justify-center w-2/5 mb-6'>
+    <div className='grid md:grid-cols-5 gap-12'>
+        <div className='relative col-span-5 md:col-span-2 mb-3'>
             <Image className='object-cover h-48 w-48 sm:h-48 sm:w-48 md:h-64 md:w-64 m-3' 
                 borderRadius='full'
                 src='/Profile.JPG'
             />
         </div>
-        <div className='w-2/5'>
+        <div className='col-span-5 md:col-span-3 '>
             <h1 className='text-3xl pb-3'>Endo Taishi</h1>
             <h3 className='text-2xl font-bold mb-3'>経歴</h3>
             <div className='mb-6'>
