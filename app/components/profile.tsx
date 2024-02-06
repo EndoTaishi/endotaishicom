350feb088de0921c0af44574342ab405b11f35b6
Taishi Endo <82435332+EndoTaishi@users.noreply.github.com>
2024-02-06 17:23:07 +0900

    Update profile component layout

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -3,14 +3,14 @@ import { Image } from '@chakra-ui/react'
 export function Profile() {
   return (
     <div className='grid md:grid-cols-5 gap-12'>
-        <div className='relative col-span-5 md:col-span-2 mb-3'>
-            <Image className='object-cover h-48 w-48 lg:h-64 lg:w-64 m-3' 
+        <div className='col-span-5 md:col-span-2 flex justify-center'>
+            <Image className='object-cover h-40 w-40 lg:h-72 lg:w-72 m-3 flex justify-center items-center' 
                 borderRadius='full'
                 src='/Profile.JPG'
             />
         </div>
         <div className='col-span-5 md:col-span-3 '>
-            <h1 className='text-3xl pb-3'>Endo Taishi</h1>
+            <h1 className='text-3xl pb-3 flex justify-center'>Endo Taishi</h1>
             <h3 className='text-2xl font-bold mb-3'>経歴</h3>
             <div className='mb-6'>
                 <p>2023/04~</p>
