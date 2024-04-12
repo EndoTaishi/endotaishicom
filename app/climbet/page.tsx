ff8cec4e3ac2e757c9b1d0cf0b1b772d10e957c2
Taishi Endo <82435332+EndoTaishi@users.noreply.github.com>
2024-04-12 20:32:57 +0900

    climbetを修正

--- app/climbet/page.tsx
+++ app/climbet/page.tsx
@@ -21,6 +21,67 @@ export default function ClimBet() {
           </p>
 
           <div className="font-roboto text-blue-900 font-bold text-lg my-4">
+            4月9日から4月13日の東京都（東京）の平均気温の平年差は2.5℃以上になるか？
+          </div>
+
+          <div className="flex flex-col bg-blue-100 p-4 rounded my-2">
+            <p className="font-roboto text-blue-800 text-sm">
+              平年値とは、西暦の一の位が1の年から30年後の一の位が0で終わる年までの、30年間分の気象データについて算出した平均値のことを言います。
+            </p>
+            <p className="font-roboto text-blue-800 text-sm">
+              平年差とは、ある期間における気象データの平均値と、同じ期間における平年値の期間平均との差分のことを指します。
+            </p>
+            <a href='https://www.data.jma.go.jp/stats/data/mdrr/tenkou/indexTenkou.html?gazou=tem5d00&dhi=0' className="font-roboto text-blue-600 text-xs mt-2">
+              ソース: <span className="underline">https://www.data.jma.go.jp/stats/data/mdrr/tenkou/indexTenkou.html?gazou=tem5d00&dhi=0</span>
+            </a>
+            <p className="font-roboto text-red-600 text-xs mt-2">
+              回答期限: 4月14日 23:59
+            </p>
+          </div>
+
+          <div>
+            <p>最終オッズ(4月12日am20:30現在)：</p>
+            <p>YES：1.0倍</p>
+            <p>NO ：1.0倍</p>
+          </div>
+
+          <div className="flex flex-col items-center my-4 mb-8">
+            <div className="flex items-center border-2 border-blue-900 rounded overflow-hidden w-full mb-4">
+              <button
+                className="bg-blue-900 text-white px-4 py-2"
+                onClick={() =>
+                  setBetAmount((prev) => (parseInt(prev) - 10).toString()) // 文字列を数値に変換してから演算し、再び文字列に変換する
+                }
+              >
+                -
+              </button>
+              <input
+                type="text"
+                name="betAmount"
+                value={betAmount}
+                onChange={handleInputChange}
+                placeholder="賭ける金額を入力してください"
+                className="flex-1 text-center bg-white text-blue-900 py-2 rounded outline-none"
+              />
+              <button
+                className="bg-blue-900 text-white px-4 py-2"
+                onClick={() =>
+                  setBetAmount((prev) => (parseInt(prev) + 10).toString()) // 文字列を数値に変換してから演算し、再び文字列に変換する
+                }
+              >
+                +
+              </button>
+            </div>
+            <a href={`https://docs.google.com/forms/d/e/1FAIpQLSeFA3O1MGYKyY7qltkar1gFz7O1gD8JRNcx-4xp5LdVFAnTTA/viewform?usp=pp_url&entry.8256214=${betAmount}`} className="flex justify-center font-roboto text-white bg-blue-900 rounded px-8 py-2 w-full">
+              賭けてみる!!
+            </a>
+          </div>
+
+          <div className="flex items-center bg-red-400 px-3 py-1 rounded-full text-sm font-roboto mb-2">
+            <span className="text-blue-900">昨日までの予想 🌐</span>
+          </div>
+
+          <div className="font-roboto text-blue-900 font-bold text-lg my-4">
             世界の二酸化炭素濃度の4月7日から4月11日の平均値は421.92ppmを超えるか？
           </div>
 
@@ -37,9 +98,9 @@ export default function ClimBet() {
           </div>
 
           <div>
-            <p>現在のオッズ(4月12日am23:59現在)：</p>
+            <p>現在のオッズ(4月12日am20:30現在)：</p>
             <p>YES：1.5倍</p>
-            <p>NO ：1.5倍</p>
+            <p>NO ：3.0倍</p>
           </div>
 
           <div className="flex flex-col items-center my-4 mb-8">
@@ -74,9 +135,7 @@ export default function ClimBet() {
             </a>
           </div>
 
-          <div className="flex items-center bg-red-400 px-3 py-1 rounded-full text-sm font-roboto mb-2">
-            <span className="text-blue-900">昨日までの予想 🌐</span>
-          </div>
+          <hr/>
 
           <div className="font-roboto text-blue-900 font-bold text-lg my-4">
             4月7日から4月11日の東京都（東京）の平均気温の平年差は2℃以上になるか？
